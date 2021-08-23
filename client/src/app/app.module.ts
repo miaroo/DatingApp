aef163207f8458b92922bf18f0d4ad1651c7e6d2
miaroo <156233@stud.prz.edu.pl>
2021-08-23 19:47:06 +0200

    ehe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     MemberListComponent,
     MemberDetailComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
